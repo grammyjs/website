665d3063a558c7adc3a6d7120942805b8e5c5b2c
KnorpelSenf <shtrog@gmail.com>
2021-06-03 11:10:48 +0200

    Revert "Test route prefetching"
    This reverts commit 272318c2a71dfa5ea0d284e739c1c627946802ca.

--- site/docs/.vuepress/clientAppEnhance.ts
+++
@@ -1,6 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-import RouterPrefetch from 'vue-router-prefetch'
-
-export default defineClientAppEnhance(({ app }) => {
-    app.use(RouterPrefetch)
-})
