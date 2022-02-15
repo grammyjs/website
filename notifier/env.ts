d82472b2f7d123d9407c5839a102f7a17d650f28
Roj <ez@roj.im>
2022-02-15 21:50:30 +0300

    Initialize the Notifier (#243)

---
+++ notifier/env.ts
@@ -0,0 +1,7 @@
+import { cleanEnv, num, str } from "https://deno.land/x/envalid@v0.0.2/mod.ts";
+
+export default cleanEnv(Deno.env.toObject(), {
+  BOT_TOKEN: str(),
+  SECRET: str(),
+  CHAT_ID: num(),
+});
