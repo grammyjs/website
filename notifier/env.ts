975b164b0d0699644253f63fadbab3f4937f8bbe
Roj <ez@roj.im>
2022-03-31 15:55:02 +0300

    Enhance the notifier (#270)

--- notifier/env.ts
+++ notifier/env.ts
@@ -1,7 +1,8 @@
-import { cleanEnv, num, str } from "https://deno.land/x/envalid@v0.0.2/mod.ts";
+import { cleanEnv, num, str } from "https://deno.land/x/envalid@v0.0.3/mod.ts";
 
 export default cleanEnv(Deno.env.toObject(), {
   BOT_TOKEN: str(),
   SECRET: str(),
   CHAT_ID: num(),
+  DATABASE_URI: str(),
 });
