0b507e8b54e37ebf8f5de6da9827b1b5b2494122
Roj <ez@roj.im>
2022-05-24 12:13:33 +0300

    Make the notifier pin the notifications
    - Removed an unused file: `languages.ts`
    - Bumped the versions.
    - Updated the GitHub utils to work with the latest version of Oak.

--- notifier/utils.ts
+++ notifier/utils.ts
@@ -1,3 +1,6 @@
+import { assert } from "https://deno.land/std@0.140.0/testing/asserts.ts";
+import { NativeRequest } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import type { ServerRequest } from "https://deno.land/x/oak@v10.6.0/types.d.ts";
 import { verify } from "https://raw.githubusercontent.com/octokit/webhooks-methods.js/v2.0.0/src/web.ts";
 import env from "./env.ts";
 
@@ -8,11 +11,19 @@ export type GitHubWebhookVerificationStatus = {
   verified: boolean;
 };
 
+// Type predicate used to access native Request instance
+// Ref: https://github.com/oakserver/oak/issues/501#issuecomment-1084046581
+function isNativeRequest(r: ServerRequest): r is NativeRequest {
+  // deno-lint-ignore no-explicit-any
+  return (r as any).request instanceof Request;
+}
+
 // Because this uses a native Request, it can be used in other contexts besides Oak (e.g. `std/http/serve`)
 export async function verifyGitHubWebhook(
-  request: Request,
+  request: ServerRequest,
 ): Promise<GitHubWebhookVerificationStatus> {
-  const id = request.headers.get("X-GitHub-Delivery");
+  assert(isNativeRequest(request));
+  const id = request.request.headers.get("X-GitHub-Delivery");
 
   // This should be more strict in reality
   if (!id) throw new Error("Not a GH webhhok");
@@ -21,7 +32,7 @@ export async function verifyGitHubWebhook(
   let verified = false;
   if (signatureHeader) {
     const signature = signatureHeader.slice("sha256=".length);
-    const payload = await request.clone().text();
+    const payload = await request.request.clone().text();
     verified = await verify(env.SECRET, payload, signature);
   }
   return { id, verified };
