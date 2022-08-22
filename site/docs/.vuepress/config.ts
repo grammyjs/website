feb1c04e5285a31e0cd794d4503fa21a8dc52735
Roz <3948961+roziscoding@users.noreply.github.com>
2022-08-22 08:57:22 -0300

    Document roziscoding/grammy-autoquote (#441)
    Co-authored-by: Ciki Momogi <git.fytyt@aleeas.com>
    Co-authored-by: WingLim <643089849@qq.com>
    Co-authored-by: Rogerio Jurado Munhoz <rogerio.munhoz@macbook-will130867.local>
    Co-authored-by: KnorpelSenf <shtrog@gmail.com>
    Co-authored-by: Roj <ez@roj.im>

--- site/docs/.vuepress/config.ts
+++ site/docs/.vuepress/config.ts
@@ -251,6 +251,10 @@ export default defineUserConfig({
                     link: "/plugins/middlewares.html",
                   },
                   {
+                    text: "Autoquote",
+                    link: "/plugins/autoquote.html",
+                  },
+                  {
                     text: "[Submit your PR!]",
                     link: "/plugins/#submitting-your-own-package-to-the-docs",
                   },
@@ -975,6 +979,10 @@ export default defineUserConfig({
                   {
                     text: "有用的中间件",
                     link: "/zh/plugins/middlewares.html",
+                  },
+                  {
+                    text: "自动引用",
+                    link: "/zh/plugins/autoquote.html",
                   },
                   {
                     text: "[等待你的 PR!]",
