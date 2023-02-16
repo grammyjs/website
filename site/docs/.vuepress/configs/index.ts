cb410879489082db33bd7894699104597300f10e
KnorpelSenf <shtrog@gmail.com>
2023-02-16 17:34:31 +0100

    chore: be consistent about no file extensions

--- site/docs/.vuepress/configs/index.ts
+++ site/docs/.vuepress/configs/index.ts
@@ -1,4 +1,4 @@
-export { docsearchEn, localeEn, siteEn } from "./en.js";
-export { docsearchEs, localeEs, siteEs } from "./es.js";
-export { docsearchId, localeId, siteId } from "./id.js";
-export { docsearchRu, localeRu, siteRu } from "./ru.js";
+export { docsearchEn, localeEn, siteEn } from "./en";
+export { docsearchEs, localeEs, siteEs } from "./es";
+export { docsearchId, localeId, siteId } from "./id";
+export { docsearchRu, localeRu, siteRu } from "./ru";
