31460cb47fb817e59c0b8fd2eebd60cdc1f0f315
Roz <roz@rjmunhoz.me>
2023-02-27 09:57:02 -0300

    feat: add getMe behavior

--- site/docs/.vuepress/components/webhook-util/TokenUi.vue
+++ site/docs/.vuepress/components/webhook-util/TokenUi.vue
@@ -1,34 +1,51 @@
 <script lang="ts" setup>
-import { computed, defineProps } from 'vue'
+import { UserFromGetMe } from '@grammyjs/types'
+import { computed, defineProps, ref } from 'vue'
 import { VAlert } from 'vuetify/components/VAlert'
 import { VBtn } from 'vuetify/components/VBtn'
 import { VCard, VCardActions, VCardText } from 'vuetify/components/VCard'
 import { VSpacer } from 'vuetify/components/VGrid'
 import { VTextField } from 'vuetify/components/VTextField'
+import { useTelegramApi } from '../../composables/use-api-method'
 import type { Translation } from './translations/types'
 
 const props = defineProps<{ strings: Translation }>()
 const translation = computed(() => props.strings.tokenCard)
+const token = ref('')
+
+const { useApiMethod } = useTelegramApi(token)
+const { refresh: getMe, state, data: botInfo, error } = useApiMethod('getMe')
+
+const emit = defineEmits<{ (event: 'info', botInfo: UserFromGetMe, token: string) }>()
+
+const loadBotInfo = async () => {
+  await getMe()
+  emit('info', botInfo.value, token.value)
+}
 </script>
 <template>
   <div class="token-ui">
-    <v-alert type="warning" :text="translation.disclaimer.content"
-      :title="translation.disclaimer.title">
+    <v-alert :type="state === 'error' ? 'error' : 'warning'"
+      :text="state !== 'error' ? translation.disclaimer.content : error.message"
+      :title="state !== 'error' ? translation.disclaimer.title : undefined">
     </v-alert>
     <v-card>
       <v-card-text>
-        <v-text-field persistent-hint :hint="translation.fields.token.placeholder" :label="translation.fields.token.label" />  
+        <v-text-field :disabled="state === 'loading'" v-model="token" persistent-hint
+          :hint="translation.fields.token.placeholder" :label="translation.fields.token.label" />
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn color="primary" variant="flat" size="small">{{ translation.buttons.loadBotInfo }}</v-btn>
+        <v-btn :loading="state === 'loading'" color="primary" variant="flat" size="small" @click="loadBotInfo">
+          {{ translation.buttons.loadBotInfo }}
+        </v-btn>
       </v-card-actions>
     </v-card>
   </div>
 </template>
 
 <style scoped>
-.token-ui > .v-alert {
+.token-ui>.v-alert {
   margin-bottom: 15px;
 }
 </style>
--- site/docs/.vuepress/components/webhook-util/WebhookUtil.vue
+++ site/docs/.vuepress/components/webhook-util/WebhookUtil.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup>
+import { UserFromGetMe } from '@grammyjs/types'
 import { usePageLang } from '@vuepress/client'
-import { computed } from 'vue'
-import TokenUi from './TokenUi.vue'
+import { computed, ref } from 'vue'
 import InfoUi from './InfoUi.vue'
+import TokenUi from './TokenUi.vue'
 import { getTranslation } from './translations'
 
 const lang = usePageLang()
 const translation = computed(() => getTranslation(lang.value))
+
+const token = ref('')
+const botInfo = ref<UserFromGetMe>()
 </script>
 <template>
   <div class="webhook-util">
-    <!-- <token-ui :strings="translation" /> -->
-    <info-ui :strings="translation" />
+    <token-ui v-if="!botInfo" :token="token" :strings="translation" @load="t => token = t" />
+    <info-ui v-else :strings="translation" />
   </div>
 </template>
 <style scoped>
