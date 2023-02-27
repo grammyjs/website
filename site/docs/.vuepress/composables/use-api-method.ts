28224737b05d740b7ec8bc64da2692a49c562326
Roz <roz@rjmunhoz.me>
2023-02-27 09:57:18 -0300

    chore: deno fmt

--- site/docs/.vuepress/composables/use-api-method.ts
+++ site/docs/.vuepress/composables/use-api-method.ts
@@ -1,43 +1,43 @@
-import { Api, GrammyError } from "grammy/web"
-import { type Ref, ref, computed } from "vue"
+import { Api, GrammyError } from "grammy/web";
+import { computed, type Ref, ref } from "vue";
 
 type FunctionPropertyOf<T> = {
-  [ P in keyof T ]: T[ P ] extends Function ? P : never
-}[ keyof T ]
+  [P in keyof T]: T[P] extends Function ? P : never;
+}[keyof T];
 
-export function useApiMethod<M extends FunctionPropertyOf<Api>> (
+export function useApiMethod<M extends FunctionPropertyOf<Api>>(
   api: Ref<Api>,
   method: M,
-  clearTimeout: number
+  clearTimeout: number,
 ) {
-  const state = ref<'idle' | 'loading' | 'success' | 'error'>('idle')
-  const data = ref<Awaited<ReturnType<Api[ M ]>>>()
-  const error = ref<GrammyError>()
+  const state = ref<"idle" | "loading" | "success" | "error">("idle");
+  const data = ref<Awaited<ReturnType<Api[M]>>>();
+  const error = ref<GrammyError>();
 
-  async function refresh (...args: Parameters<Api[ M ]>) {
-    data.value = undefined
-    error.value = undefined
+  async function refresh(...args: Parameters<Api[M]>) {
+    data.value = undefined;
+    error.value = undefined;
 
-    const callable = api.value[ method ]
+    const callable = api.value[method];
 
-    state.value = 'loading'
+    state.value = "loading";
     await callable
       .apply(api.value, args)
-      .then((result: Awaited<ReturnType<Api[ M ]>>) => {
-        data.value = result
-        state.value = 'success'
+      .then((result: Awaited<ReturnType<Api[M]>>) => {
+        data.value = result;
+        state.value = "success";
       })
       .catch((err) => {
-        error.value = err
-        state.value = 'error'
+        error.value = err;
+        state.value = "error";
       })
       .finally(() => {
         if (clearTimeout) {
           setTimeout(() => {
-            state.value = 'idle'
-          }, clearTimeout)
+            state.value = "idle";
+          }, clearTimeout);
         }
-      })
+      });
   }
 
   return {
@@ -45,13 +45,16 @@ export function useApiMethod<M extends FunctionPropertyOf<Api>> (
     error,
     data,
     refresh,
-  }
+  };
 }
 
-export function useTelegramApi (token: Ref<string>) {
-  const api = computed(() => new Api(token.value))
+export function useTelegramApi(token: Ref<string>) {
+  const api = computed(() => new Api(token.value));
 
   return {
-    useApiMethod: <M extends FunctionPropertyOf<Api>> (method: M, clearTimeout = 0) => useApiMethod<M>(api, method, clearTimeout)
-  }
+    useApiMethod: <M extends FunctionPropertyOf<Api>>(
+      method: M,
+      clearTimeout = 0,
+    ) => useApiMethod<M>(api, method, clearTimeout),
+  };
 }
