8a6a76a19e21fdb40754c263c98830b5ccad858a
Roz <roz@rjmunhoz.me>
2023-03-02 09:15:35 -0300

    feat: remove button to load bot info

--- site/docs/.vuepress/components/webhook-util/TokenUi.vue
+++ site/docs/.vuepress/components/webhook-util/TokenUi.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { UserFromGetMe } from '@grammyjs/types'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { VAlert } from 'vuetify/components/VAlert'
 import { VBtn } from 'vuetify/components/VBtn'
 import { VCard, VCardActions, VCardText } from 'vuetify/components/VCard'
@@ -8,6 +8,7 @@ import { VSpacer } from 'vuetify/components/VGrid'
 import { VTextField } from 'vuetify/components/VTextField'
 import { useTelegramApi } from 'grammy-vue'
 import type { Translation } from './translations/types'
+import debounce from 'lodash.debounce'
 
 const props = defineProps<{ strings: Translation }>()
 const translation = computed(() => props.strings.tokenCard)
@@ -18,10 +19,12 @@ const { refresh: getMe, state, data: botInfo, error } = useApiMethod('getMe')
 
 const emit = defineEmits<{ (event: 'info', botInfo: UserFromGetMe, token: string) }>()
 
-const loadBotInfo = async () => {
+const loadBotInfo = debounce(async () => {
   await getMe()
   emit('info', botInfo.value, token.value)
-}
+}, 10)
+
+watch(token, () => loadBotInfo())
 </script>
 <template>
   <div class="token-ui">
@@ -31,15 +34,9 @@ const loadBotInfo = async () => {
     </v-alert>
     <v-card>
       <v-card-text>
-        <v-text-field :disabled="state === 'loading'" v-model="token" persistent-hint
+        <v-text-field :loading="state === 'loading'" :disabled="state === 'loading'" v-model="token" persistent-hint
           :hint="translation.fields.token.placeholder" :label="translation.fields.token.label" />
       </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn :loading="state === 'loading'" color="primary" variant="flat" size="small" @click="loadBotInfo">
-          {{ translation.buttons.loadBotInfo }}
-        </v-btn>
-      </v-card-actions>
     </v-card>
   </div>
 </template>
