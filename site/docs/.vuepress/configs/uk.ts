ea6fbb51275c27c0b27b4d569678c740eff54065
Nazar Antoniuk <75927667+Nazar-Ant@users.noreply.github.com>
2023-03-08 20:38:07 +0200

    Update site/docs/.vuepress/configs/uk.ts

--- site/docs/.vuepress/configs/uk.ts
+++ site/docs/.vuepress/configs/uk.ts
@@ -57,7 +57,7 @@ export const localeUk: LocaleConfig<DefaultThemeLocaleData> = {
                 link: "/uk/guide/api.html",
               },
               {
-                text: "Запити фільтрування і bot.on()",
+                text: "Запити фільтрування та bot.on()",
                 link: "/uk/guide/filter-queries.html",
               },
               {
