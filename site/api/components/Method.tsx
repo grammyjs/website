a5568392f913f3edc53bda2016bad2cf4ab92223
Roj <ez@roj.im>
2023-11-04 11:13:57 +0300

    Fix links

--- site/api/components/Method.tsx
+++ site/api/components/Method.tsx
@@ -1,9 +1,4 @@
-import {
-  ClassMethodDef,
-  DocNodeInterface,
-  ParamIdentifierDef,
-} from "deno_doc/types.d.ts";
-import { PropertyName } from "./PropertyName.tsx";
+import { ClassMethodDef } from "deno_doc/types.d.ts";
 import { Params, TsType } from "./TsType.tsx";
 import { LinkGetter } from "./types.ts";
 import { CodeBlock } from "./CodeBlock.tsx";
@@ -35,12 +30,12 @@ export function Method({
             ""
           )}
         <span style="color: #62E884">{method.name}</span>(
-        <Params getLink={() => null}>{method.functionDef.params}</Params>)
+        <Params getLink={getLink}>{method.functionDef.params}</Params>)
         {method.functionDef.returnType
           ? (
             <span>
               :{" "}
-              <TsType getLink={() => null}>
+              <TsType getLink={getLink}>
                 {method.functionDef.returnType}
               </TsType>
             </span>
