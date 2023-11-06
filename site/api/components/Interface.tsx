640ed998c7b73609eae890fdf336f942f6d2b6f3
Roj <ez@roj.im>
2023-11-06 22:20:47 +0300

    Show what an interface extends

--- site/api/components/Interface.tsx
+++ site/api/components/Interface.tsx
@@ -4,8 +4,9 @@ import { H1 } from "./H1.tsx";
 import { P } from "./P.tsx";
 import { LinkGetter } from "./types.ts";
 import { Sector } from "./Sector.tsx";
-
 import { Loc } from "./Loc.tsx";
+import { CodeBlock } from "./CodeBlock.tsx";
+import { TsType, TypeRef } from "./TsType.tsx";
 
 export function Interface(
   { children: iface, getLink }: {
@@ -20,6 +21,16 @@ export function Interface(
       <H1>{iface.name}</H1>
       <P doc>{iface.jsDoc?.doc}</P>
       <Loc>{iface}</Loc>
+      <Sector title="Extends" show={!!iface.interfaceDef.extends.length}>
+        <CodeBlock>
+          {(() => {
+            const a = iface.interfaceDef.extends.map((v) => (
+              <TsType getLink={getLink}>{v}</TsType>
+            ));
+            return a.length > 0 ? a.reduce((a, b) => <>{a}, {b}</>) : null;
+          })()}
+        </CodeBlock>
+      </Sector>
       <Sector title="Properties" show={!!props.length}>
         <Properties getLink={getLink}>{props}</Properties>
       </Sector>
