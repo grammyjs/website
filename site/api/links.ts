3b425a725608de588b180368129db707f32ae96c
KnorpelSenf <shtrog@gmail.com>
2023-11-21 14:51:21 +0100

    Add more links

--- site/api/links.ts
+++ site/api/links.ts
@@ -1,3 +1,18 @@
 export default {
+  // deno.land/api
+  "Deno.FsFile": "https://deno.land/api@v1.38.2?s=Deno.FsFile",
+  // MDN
   "AbortSignal": "https://developer.mozilla.org/docs/Web/API/AbortSignal",
+  "AsyncIterable":
+    "https://developer.mozilla.org/docs/Web/JavaScript/Reference/Iteration_protocols#the_async_iterator_and_async_iterable_protocols",
+  "Blob": "https://developer.mozilla.org/docs/Web/API/Blob",
+  "Iterable":
+    "https://developer.mozilla.org/docs/Web/JavaScript/Reference/Iteration_protocols#the_iterator_protocol",
+  "Promise":
+    "https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Promise",
+  "ReadableStream": "https://developer.mozilla.org/docs/Web/API/ReadableStream",
+  "Response": "https://developer.mozilla.org/docs/Web/API/Response",
+  "Uint8Array":
+    "https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array",
+  "URL": "https://developer.mozilla.org/docs/Web/API/URL",
 } as Record<string, string | undefined>;
