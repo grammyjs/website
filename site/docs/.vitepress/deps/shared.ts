869f201762c29c27ad1c8df834f7f90932edc93c
quadratz <quadratz@proton.me>
2024-02-15 09:07:11 +0700

    Fix build error
    Error: `Rollup failed to resolve import "mrmime"`

--- site/docs/.vitepress/deps/shared.ts
+++ site/docs/.vitepress/deps/shared.ts
@@ -1,7 +1,8 @@
 export const EXTERNAL_URL_RE = /^(?:[a-z]+:|\/\/)/i;
 export const APPEARANCE_KEY = "vitepress-theme-appearance";
-export const HASH_RE = /#.*$/;
-export const EXT_RE = /(index)?\.(md|html)$/;
+const HASH_RE = /#.*$/;
+const HASH_OR_QUERY_RE = /[?#].*$/;
+const INDEX_OR_EXT_RE = /(?:(^|\/)index)?\.(?:md|html)$/;
 export const inBrowser = typeof document !== "undefined";
 export const notFoundPageData = {
   relativePath: "",
@@ -13,11 +14,7 @@ export const notFoundPageData = {
   lastUpdated: 0,
   isNotFound: true,
 };
-export function isActive(
-  currentPath: string,
-  matchPath: string,
-  asRegex = false,
-) {
+export function isActive(currentPath, matchPath, asRegex = false) {
   if (matchPath === undefined) {
     return false;
   }
@@ -34,35 +31,18 @@ export function isActive(
   }
   return true;
 }
-export function normalize(path: string) {
-  return decodeURI(path).replace(HASH_RE, "").replace(EXT_RE, "");
+function normalize(path) {
+  return decodeURI(path)
+    .replace(HASH_OR_QUERY_RE, "")
+    .replace(INDEX_OR_EXT_RE, "$1");
 }
-export function isExternal(path: string) {
+export function isExternal(path) {
   return EXTERNAL_URL_RE.test(path);
 }
 /**
  * this merges the locales data to the main data by the route
  */
-export function resolveSiteDataByRoute(siteData: {
-  locales: {
-    [x: string]: {
-      lang: any;
-      dir: any;
-      title: any;
-      titleTemplate: any;
-      description: any;
-      head: never[];
-      themeConfig: any;
-    };
-  };
-  lang: any;
-  dir: any;
-  title: any;
-  titleTemplate: any;
-  description: any;
-  head: any;
-  themeConfig: any;
-}, relativePath: any) {
+export function resolveSiteDataByRoute(siteData, relativePath) {
   const localeIndex =
     Object.keys(siteData.locales).find((key) =>
       key !== "root" &&
@@ -88,19 +68,19 @@ export function resolveSiteDataByRoute(siteData: {
 /**
  * Create the page title string based on config.
  */
-export function createTitle(
-  siteData: { title: any; titleTemplate: any },
-  pageData: { title: any; titleTemplate: any },
-) {
+export function createTitle(siteData, pageData) {
   const title = pageData.title || siteData.title;
   const template = pageData.titleTemplate ?? siteData.titleTemplate;
   if (typeof template === "string" && template.includes(":title")) {
     return template.replace(/:title/g, title);
   }
   const templateString = createTitleTemplate(siteData.title, template);
+  if (title === templateString.slice(3)) {
+    return title;
+  }
   return `${title}${templateString}`;
 }
-function createTitleTemplate(siteTitle: any, template: boolean | undefined) {
+function createTitleTemplate(siteTitle, template) {
   if (template === false) {
     return "";
   }
@@ -112,7 +92,7 @@ function createTitleTemplate(siteTitle: any, template: boolean | undefined) {
   }
   return ` | ${template}`;
 }
-function hasTag(head: [any, any][], tag: [any, any]) {
+function hasTag(head, tag) {
   const [tagType, tagAttrs] = tag;
   if (tagType !== "meta") {
     return false;
@@ -125,13 +105,13 @@ function hasTag(head: [any, any][], tag: [any, any]) {
     type === tagType && attrs[keyAttr[0]] === keyAttr[1]
   );
 }
-export function mergeHead(prev: any[], curr: any) {
-  return [...prev.filter((tagAttrs: any) => !hasTag(curr, tagAttrs)), ...curr];
+export function mergeHead(prev, curr) {
+  return [...prev.filter((tagAttrs) => !hasTag(curr, tagAttrs)), ...curr];
 }
 // https://github.com/rollup/rollup/blob/fec513270c6ac350072425cc045db367656c623b/src/utils/sanitizeFileName.ts
 const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
 const DRIVE_LETTER_REGEX = /^[a-z]:/i;
-export function sanitizeFileName(name: string) {
+export function sanitizeFileName(name) {
   const match = DRIVE_LETTER_REGEX.exec(name);
   const driveLetter = match ? match[0] : "";
   return (driveLetter +
@@ -140,6 +120,31 @@ export function sanitizeFileName(name: string) {
       .replace(INVALID_CHAR_REGEX, "_")
       .replace(/(^|\/)_+(?=[^/]*$)/, "$1"));
 }
-export function slash(p: string) {
+export function slash(p) {
   return p.replace(/\\/g, "/");
+}
+const KNOWN_EXTENSIONS = new Set();
+export function treatAsHtml(filename) {
+  if (KNOWN_EXTENSIONS.size === 0) {
+    //@ts-ignore
+    const extraExts =
+      (typeof process === "object" && process.env.VITE_EXTRA_EXTENSIONS) ||
+      import.meta.env?.VITE_EXTRA_EXTENSIONS ||
+      "";
+    ("3g2,3gp,aac,ai,apng,au,avif,bin,bmp,cer,class,conf,crl,css,csv,dll," +
+      "doc,eps,epub,exe,gif,gz,ics,ief,jar,jpe,jpeg,jpg,js,json,jsonld,m4a," +
+      "man,mid,midi,mjs,mov,mp2,mp3,mp4,mpe,mpeg,mpg,mpp,oga,ogg,ogv,ogx," +
+      "opus,otf,p10,p7c,p7m,p7s,pdf,png,ps,qt,roff,rtf,rtx,ser,svg,t,tif," +
+      "tiff,tr,ts,tsv,ttf,txt,vtt,wav,weba,webm,webp,woff,woff2,xhtml,xml," +
+      "yaml,yml,zip" +
+      (extraExts && typeof extraExts === "string" ? "," + extraExts : ""))
+      .split(",")
+      .forEach((ext) => KNOWN_EXTENSIONS.add(ext));
+  }
+  const ext = filename.split(".").pop();
+  return ext == null || !KNOWN_EXTENSIONS.has(ext.toLowerCase());
+}
+// https://github.com/sindresorhus/escape-string-regexp/blob/ba9a4473850cb367936417e97f1f2191b7cc67dd/index.js
+export function escapeRegExp(str) {
+  return str.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&").replace(/-/g, "\\x2d");
 }
