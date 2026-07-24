5d22bfe3fa63c3c8d5c4a178fb2f007de2bb80c2
KnorpelSenf <shtrog@gmail.com>
2026-07-24 22:52:51 +0200

    Fix trailing comma

--- site/docs/.vitepress/plugins/markdown.ts
+++ site/docs/.vitepress/plugins/markdown.ts
@@ -1,5 +1,5 @@
 import type MarkdownIt from "$types/markdown-it";
-import { betterLineBreaks, } from "./index.ts";
+import { betterLineBreaks } from "./index.ts";
 
 export const markdown = (md: MarkdownIt) => {
   md.use(betterLineBreaks);
